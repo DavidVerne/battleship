f9f5e4d193bf9f4e051d193159180b0ffa86977f
David Verne <davidverne@davids-air.home>
2019-05-31 21:40:45 +0100

    Updates

---
+++ precache-manifest.95ddb97d7d3701ea40d59b982457a230.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59583d304e3024273286aafd7c809f70",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "1091474a500e754abb78",
+    "url": "/battleship/static/css/main.0e4d310e.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/battleship/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "1091474a500e754abb78",
+    "url": "/battleship/static/js/main.beb83e7a.chunk.js"
+  },
+  {
+    "revision": "c8c12a64d41e1b219556",
+    "url": "/battleship/static/js/runtime~main.26ba8606.js"
+  },
+  {
+    "revision": "cc0a7c677a289334d173928dfac02605",
+    "url": "/battleship/static/media/board.cc0a7c67.jpg"
+  }
+]);
